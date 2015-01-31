96cddcfc940f10214cf1d7a7863793f3b9bfe395
Tobias_Conrad <Tobias_Conrad@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-31 17:31:36 +0000

    moved
    git-svn-id: https://plugins.svn.wordpress.org/wp-to-klick-tipp-tag-basiertes-e-mail-marketing/trunk@1079675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/assets/js/general.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-    
-    // show activity indicator
-    jQuery('.show-activity-indicator').click(function() {
-        jQuery(this).parent().find('img').show();
-    });
-});
